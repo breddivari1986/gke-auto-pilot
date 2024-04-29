c4c2dc93b16e1bca7dca597d4b1e2045f9ec3bdf
jiten-kumar <jiten.kumar@gmail.com>
2024-04-29 14:57:40 -0400

    updated files

--- code/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "gcs" {
-    // bucket = var.backend_gcs_bucket // Variables not allowed in backend
-    // prefix = var.backend_gcs_bucket_prefix // Variables not allowed in backend
-    # bucket = "<bucket name : test-terraformstates>"
-    # prefix = "anthos/gke"
-  }
-}
